595ea7f7ecbea705fd5fffa01309aaaacb1f1e8e
Marçal Pla <marcal@taleia.software>
2016-10-29 12:00:56 +0200

    Upgrade to EMR 5.0.0 and some minor improvements.

--- aws_launch_spark_app.sh
+++ aws_launch_spark_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Launch AWS EMR Spark Application using aws cli
-# Usage: ./aws_launch_spark_app.sh CLUSTER_NAME INSTANCE_TYPE INSTANCE_COUNT LOG_URI CONFIG_FILE [-s step_class step_class_jar [step_class_params]...]...
+# Usage: ./aws_launch_spark_app.sh CLUSTER_NAME INSTANCE_TYPE INSTANCE_COUNT LOG_URI CONFIG_FILE [-s step_class_full_name step_class_jar [step_class_params]...]...
 
 CLUSTER_NAME=$1
 INSTANCE_TYPE=$2
@@ -14,7 +14,7 @@ STEPS=""
 for arg in "$@"; do
   shift
   if [ $arg = '-s' ]; then
-    step_class=$1
+    step_class_full_name=$1
     step_class_jar=$2
     step_class_params_array=${@:3}
     step_class_params=""
@@ -27,7 +27,7 @@ for arg in "$@"; do
       step_class_params+=",$step_class_param"
     done
 
-    step="$step_class,$step_class_jar$step_class_params"
+    step="$step_class_full_name,$step_class_jar$step_class_params"
 
     # Build the step name
     # Include step class params with length < 20
@@ -40,7 +40,7 @@ for arg in "$@"; do
     step_class_params=${step_class_params//$char_class_rep+($char_class)/}
     step_class_params=${step_class_params//,/}
     # Include step class
-    step_name="$step_class$step_class_params"
+    step_name="$step_class_full_name$step_class_params"
 
     STEPS+="Type=Spark,Name=$step_name,Args=[--deploy-mode,cluster,--master,yarn-cluster,--class,$step] "
   fi
@@ -48,10 +48,11 @@ done
 
 aws emr create-cluster \
 --name $CLUSTER_NAME \
---release-label emr-4.2.0 \
+--release-label emr-5.0.0 \
 --instance-type $INSTANCE_TYPE \
 --instance-count $INSTANCE_COUNT \
---ec2-attributes KeyName=emndata \
+--service-role EMR_DefaultRole \
+--ec2-attributes KeyName=define_key_name,InstanceProfile=EMR_EC2_DefaultRole \
 --applications Name=Spark \
 --log-uri $LOG_URI \
 --configurations $CONFIG_FILE \
